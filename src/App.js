343784d5ad630266b0e04c31757253985bc040d4
kiiingkoooy <vladimirtudtud@gmail.com>
2023-02-21 10:29:34 +0800

    Header, Default, Home, Landing, Weather page creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Default from "./pages/Default";
+//import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header />
+      <Routes>
+        <Route path="/" element={<Default />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Default.js
@@ -0,0 +1,7 @@
+const Default = () => {
+    return (
+        <div className="">Hello, World!</div>
+    )
+}
+
+export default Default;
