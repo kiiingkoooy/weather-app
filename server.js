3d64e66495da34dac3a93b6d4865ecdb6e89b4d4
kiiingkoooy <vladimirtudtud@gmail.com>
2023-02-21 21:45:01 +0800

    Node creation

---
+++ server.js
@@ -0,0 +1,59 @@
+let express = require("express");
+let cors = require("cors");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+let bodyParser = require("body-parser");
+
+const CLIENT_ID = "4dfacd65d4ff84914779";
+const CLIENT_SECRET = "e6a06783389b76de20bf6f3c2f50657306e296f6";
+
+let app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/getAccessToken", async function (req, res) {
+  const params =
+    "?client_id=" +
+    CLIENT_ID +
+    "&client_secret=" +
+    CLIENT_SECRET +
+    "&code=" +
+    req.query.code;
+
+  await fetch("https://github.com/login/oauth/access_token" + params, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+app.get("/getUserData", async (req, res) => {
+  req.get("Authorization"); //Access Token
+  await fetch("https://api.github.com/user", {
+    method: "GET",
+    headers: {
+      Authorization: req.get("Authorization"), //Access Token
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
+});
+
+app.listen(4000, () => {
+  console.log("CORS server running on port 4000");
+});
