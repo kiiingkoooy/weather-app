2f8b5423235398fee395005ec35915e1427cc322
kiiingkoooy <vladimirtudtud@gmail.com>
2023-02-22 07:49:41 +0800

    Routing adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
 import Store from "./store/Store";
 import Weather from "./pages/Weather";
 import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/home" element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
           <Route path="/weather" element={<PrivateRoute />}>
             <Route path="/weather" element={<Weather />} />
           </Route>
