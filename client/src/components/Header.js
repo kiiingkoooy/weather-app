e680f17613bc5b1295a5ce0a1d4c2d8dcc24016e
kiiingkoooy <vladimirtudtud@gmail.com>
2023-02-22 04:07:58 +0800

    Adding the cloud icon and navigation for Weather forecast

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "../store/Store";
+import { BsFillCloudyFill } from "react-icons/bs";
 
 const Header = () => {
   const { isLoggedIn, setIsLoggedIn } = useLogin();
@@ -14,10 +15,18 @@ const Header = () => {
   return (
     <div className="bg-blue-600 border-b shadow-sm top-0 z-40">
       <header className="py-5 px-2">
-        <ul className="flex justify-between mr-5 ml-5">
-          <li>Weather Forecast</li>
+        <ul className="flex justify-between items-center mr-5 ml-5">
+          <div className="flex cursor-pointer" onClick={() => navigate("/")}>
+            <BsFillCloudyFill className="text-[50px] text-blue-200" />
+            <li className="flex ml-3 items-center text-[25px] text-blue-100 font-semibold">
+              Weather Forecast
+            </li>
+          </div>
           {localStorage.getItem("accessToken") !== null && (
-            <li className="cursor-pointer" onClick={logOutHandler}>
+            <li
+              className="flex cursor-pointer text-[20px] text-blue-100 font-semibold"
+              onClick={logOutHandler}
+            >
               Logout
             </li>
           )}
